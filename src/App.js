452b6684face2f769457b93b40c3120af7e58c84
shijie liu <shijieliu128@gmail.com>
2021-05-06 02:07:12 -0700

    use access token connect spotify and app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from 'react'
-import Login from './Login';
+import React, { useState, useEffect } from "react";
+import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import "./App.scss";
+import Player from './Player';
 
-import './App.scss';
+const spotify = new SpotifyWebApi();
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("👦", user);
+      });
+    }
+
+    console.log("I have a token 👉 ", token);
+  }, []);
+
   return (
-      // BEM
-    <div className="app">
-    <Login/>
-    </div>
+    // BEM
+    <div className="app">{token ? <Player /> : <Login />}</div>
   );
 }
 
